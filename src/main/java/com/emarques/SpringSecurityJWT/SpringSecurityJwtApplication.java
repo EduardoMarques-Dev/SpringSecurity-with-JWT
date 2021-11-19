7fabed03d5dc525c63ff73dcf60b3116d14b1d96
Carlos Eduardo <e.marquesdev@gmail.com>
2021-11-18 21:51:55 -0300

    feat: Password Encoder declaration

--- src/main/java/com/emarques/SpringSecurityJWT/SpringSecurityJwtApplication.java
+++ src/main/java/com/emarques/SpringSecurityJWT/SpringSecurityJwtApplication.java
@@ -3,6 +3,9 @@ package com.emarques.SpringSecurityJWT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringSecurityJwtApplication {
@@ -11,4 +14,9 @@ public class SpringSecurityJwtApplication {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 }
