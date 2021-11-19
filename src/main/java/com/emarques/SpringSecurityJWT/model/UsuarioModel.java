a5f4515b61cb623cecb5ef62fc3d49cd90682f52
e.marquesdev <e.marquesdev@gmail.com>
2021-11-19 15:38:38 -0300

    feat: removing password from response

--- src/main/java/com/emarques/SpringSecurityJWT/model/UsuarioModel.java
+++ src/main/java/com/emarques/SpringSecurityJWT/model/UsuarioModel.java
@@ -1,5 +1,6 @@
 package com.emarques.SpringSecurityJWT.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class UsuarioModel {
     private Integer id;
     @Column(unique = true)
     private String login;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
 }
