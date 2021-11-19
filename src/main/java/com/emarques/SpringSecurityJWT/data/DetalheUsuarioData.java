433a1011eaaff3a200d48beb88b5c94392682a3f
e.marquesdev <e.marquesdev@gmail.com>
2021-11-19 13:55:50 -0300

    feat: creation of the class that implements UserDetais

---
+++ src/main/java/com/emarques/SpringSecurityJWT/data/DetalheUsuarioData.java
@@ -0,0 +1,51 @@
+package com.emarques.SpringSecurityJWT.data;
+
+import com.emarques.SpringSecurityJWT.model.UsuarioModel;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class DetalheUsuarioData implements UserDetails {
+
+    private final Optional<UsuarioModel> usuario;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.orElse(new UsuarioModel()).getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.orElse(new UsuarioModel()).getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
